36e7f4e7bf361ac596ea119b9850f24313365c60
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-27 22:18:36 -0300

    Add initial Symfony project configuration and assets
    Este commit inclui a configuração inicial de vários pacotes do Symfony e a adição de arquivos de ativos. Isso inclui a configuração de pacotes como doctrine, monolog, messenger, e a adição de arquivos CSS e JS, além de templates Twig.

---
+++ importmap.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Returns the importmap for this application.
+ *
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ *
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ *
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
+return [
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
+    'datatables.net-dt' => [
+        'version' => '2.1.2',
+    ],
+    'jquery' => [
+        'version' => '3.7.1',
+    ],
+    'datatables.net' => [
+        'version' => '2.1.2',
+    ],
+    'datatables.net-dt/css/dataTables.dataTables.min.css' => [
+        'version' => '2.1.2',
+        'type' => 'css',
+    ],
+    'datatables.net-plugins/i18n/pt-BR.mjs' => [
+        'version' => '2.0.8',
+    ],
+];

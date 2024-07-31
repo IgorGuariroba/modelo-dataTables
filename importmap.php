76a8a8754685e8855c3e5ebf79f4b70fb126f6c7
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-31 13:00:51 -0300

    Add Bootstrap to the project
    This commit includes Bootstrap version 5.3.3 and Popper.js version 2.11.8 in importmap.php. Additionally, it imports Bootstrap's CSS in app.js to provide styling components.

--- importmap.php
+++ importmap.php
@@ -41,4 +41,14 @@ return [
     'datatables.net-plugins/i18n/pt-BR.mjs' => [
         'version' => '2.0.8',
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
